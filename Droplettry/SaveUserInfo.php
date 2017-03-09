14147abb15360ee03275a24d35a49341960c5daa
Zixun Xiang <zxiang@fullsail.edu>
2017-03-09 10:46:51 -0500

    save user info

---
+++ Droplettry/SaveUserInfo.php
@@ -0,0 +1,21 @@
+<?php
+$txt = "data.def"; 
+if (isset($_POST['username']) && isset($_POST['uiname'])) { // check if both fields are set
+    $fh = fopen($txt, 'wa+'); 
+
+	//making .def file
+    $txt='entityDef '.$_POST['uiname'].'{
+	"inherit"		"bot"
+	
+	"author"		"'.$_POST['username'].'"
+	"bot_type"		"Script"
+	"scriptclass"	"'.$_POST['uiname'].'"
+	
+	"ui_name"		"'.$_POST['uiname'].'"
+	}'; 
+
+
+    fwrite($fh,$txt); 
+    fclose($fh); 
+}
+?>
